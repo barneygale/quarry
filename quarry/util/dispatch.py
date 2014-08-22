a55238f71c5a37a2be5c6f0364ccba71d887e89c
Barney Gale <barney.gale@gmail.com>
2014-08-23 00:44:10 +0100

    Add utility dump_packet method

--- quarry/util/dispatch.py
+++ quarry/util/dispatch.py
@@ -1,3 +1,4 @@
+import string
 
 # Registers a packet handler by giving it a '_packet_handler' field
 def register(*lookup_args):
@@ -23,3 +24,28 @@ class PacketDispatcher(object):
             getattr(self, handler)(buff)
             return True
         return False
+
+    def dump_packet(self, data):
+        lines = ['Packet dump:']
+        bytes_read = 0
+        while len(data) > 0:
+            data_line, data = data[:16], data[16:]
+
+            l_hex = []
+            l_str = []
+            for c in data_line:
+                l_hex.append("%02x" % ord(c))
+                l_str.append(c if c in string.printable else ".")
+
+            l_hex.extend(['  ']*(16-len(l_hex)))
+            l_hex.insert(8,'')
+
+
+            lines.append("%08x  %s  |%s|" % (
+                bytes_read,
+                " ".join(l_hex),
+                "".join(l_str)))
+
+            bytes_read += len(data_line)
+
+        return "\n    ".join(lines + ["%08x" % bytes_read])
