dd5c77b9431e8651076b60bff37e4ba56b389daf
Barney Gale <barney.gale@gmail.com>
2017-02-11 14:38:24 +0000

    Bump version to 0.6.2

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ copyright = u'2015, Barney Gale'
 # built documents.
 #
 # The short X.Y version.
-version = '0.6.1'
+version = '0.6.2'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='quarry',
-    version='0.6.1',
+    version='0.6.2',
     author='Barney Gale',
     author_email='barney@barneygale.co.uk',
     url='https://github.com/barneygale/quarry',
