02695039422868ac00217369c8594b955327f502
Ethan Wong <epicface92@gmail.com>
2019-08-14 02:28:52 -0500

    Bump version to 1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='quarry',
-    version='1.3',
+    version='1.4',
     author='Barney Gale',
     author_email='barney@barneygale.co.uk',
     url='https://github.com/barneygale/quarry',
