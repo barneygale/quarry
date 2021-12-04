eac47471fc55598de6b4723a728a37d035002237
Dries <admin@dries007.net>
2021-12-04 16:15:29 +0100

    Bump version for 1.18

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='quarry',
-    version='1.7.0',
+    version='1.8.0',
     author='Barney Gale',
     author_email='barney@barneygale.co.uk',
     url='https://github.com/barneygale/quarry',
